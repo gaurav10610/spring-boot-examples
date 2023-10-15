2949c581d170562b2e67f175ac9a6ba0304a9226
Gaurav Kumar Yadav <gaurav10610@gmail.com>
2023-10-15 15:22:44 +0530

    minor checkstyle fix

--- src/main/java/com/example/springboot/controller/BaseController.java
+++ src/main/java/com/example/springboot/controller/BaseController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 
 public class BaseController {
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	protected ResponseEntity<?> buildResponse(Object object) {
 		return new ResponseEntity(object, HttpStatusCode.valueOf(200));
 	}
